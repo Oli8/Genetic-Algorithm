c0f0ae0004be89916f6e168a536493b63c5b41be
Olivier Crete <olivier.crete@epitech.eu>
2017-06-13 00:28:14 +0200

    crossover method

--- chromosome.rb
+++ chromosome.rb
@@ -21,19 +21,21 @@ class Chromosome
 		@value[rand(0...@value.size)] = ALPHABET.sample
 	end
 
+	def crossover(chromosome)
+		# @value[0..@size / 2 - 1] + chromosome[@size / 2..-1]
+		half = @size / 2
+		(@value.chars.shuffle.take(half) + chromosome.chars.shuffle.take(half.even? ? half : half + 1)).join
+	end
 end
 
 def random_string(size)
-	alphabet = [*'a'..'z', *'A'..'Z']
 	output = ""
 	size.times do 
-		output += alphabet.sample
+		output += ALPHABET.sample
 	end
 	output
 end 
 
 c = Chromosome.new(7)
 p c.value
-#p c.score("olivier")
-c.mutate
-p c.value
+c.crossover('azertyu')
