07da8d0579484be659a46521420d68329918ae3b
Olivier Crete <olivier.crete@epitech.eu>
2017-06-19 19:39:14 +0200

    CLI options

--- main.rb
+++ main.rb
@@ -1,4 +1,26 @@
+require 'optparse'
 require_relative 'genetic_algo'
 require_relative 'chromosome'
 
-Genetic_algo.new(ARGV[0] || abort("You must provide a solution"), ARGV[1] || 100).run
+options = {}
+
+optparse = OptionParser.new do |opts|
+	opts.banner = "Usage: ruby main.rb [options] solution"
+
+	options[:population] = 100
+	opts.on( '-p', '--population SIZE', Integer, 'Set the population size.') do |size|
+		options[:population] = size
+	end
+
+	options[:max_time] = false
+	opts.on( '-m', '--max_time MS', Integer, 'Stop the process after number of milliseconds set.') do |ms|
+		options[:max_time] = ms
+	end
+
+	opts.on( '-h', '--help', 'Display help.' ) do
+		puts opts
+		exit
+	end
+end.parse!
+
+Genetic_algo.new(ARGV.last || abort("You must provide a solution."), options[:population]).run
